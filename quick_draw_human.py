87b48ea39e945292bd58b688f065935a3a8005a1
jarminet <0710082@galileo.edu>
2020-08-08 04:44:51 -0600

    Testing

--- quick_draw_human.py
+++ quick_draw_human.py
@@ -1,7 +1,7 @@
-class_names = ['cloud', 'sun', 'pants', 'umbrella', 'table', 'ladder', 'eyeglasses', 'clock', 'scissors', 'cup']
+class_names = ['pants', 'eyeglasses']
 
 import urllib.request
-import os 
+import os
 import numpy as np
 from sklearn.manifold import TSNE
 # We'll use matplotlib for graphics.
@@ -30,12 +30,12 @@ def download_and_load(test_split = 0.2, max_items_per_class = 10000):
     print(path)
     urllib.request.urlretrieve(path, f'{root}/{c}.npy')
   print('loading ...')
-  
-  #initialize variables 
+
+  #initialize variables
   x = np.empty([0, 784])
   y = np.empty([0])
 
-  #load each data file 
+  #load each data file
   for idx, file in enumerate(class_names):
       data = np.load(f'{root}/{file}.npy')
       data = data[0: max_items_per_class, :]
@@ -47,15 +47,15 @@ def download_and_load(test_split = 0.2, max_items_per_class = 10000):
   data = None
   labels = None
 
-  #randomize the dataset 
+  #randomize the dataset
   permutation = np.random.permutation(y.shape[0])
   x = x[permutation, :]
   y = y[permutation]
 
-  #reshape and inverse the colors 
+  #reshape and inverse the colors
   x = 255 - np.reshape(x, (x.shape[0], 28, 28))
 
-  #separate into training and testing 
+  #separate into training and testing
   test_size  = int(x.shape[0]/100*(test_split*100))
 
   x_test = x[0:test_size, :]
@@ -63,7 +63,7 @@ def download_and_load(test_split = 0.2, max_items_per_class = 10000):
 
   x_train = x[test_size:x.shape[0], :]
   y_train = y[test_size:y.shape[0]]
-  
+
   print('Training Data : ', x_train.shape[0])
   print('Testing  Data : ', x_test.shape[0])
   return x_train, y_train, x_test, y_test, class_names
@@ -84,7 +84,7 @@ def scatter(x, colors):
 
     # We add the labels for each digit.
     txts = []
-    for i in range(10):
+    for i in range(2):
         # Position of each label.
         xtext, ytext = np.median(x[colors == i, :], axis=0)
         txt = ax.text(xtext, ytext, class_names[i], fontsize=15)
@@ -92,7 +92,7 @@ def scatter(x, colors):
             PathEffects.Stroke(linewidth=5, foreground="w"),
             PathEffects.Normal()])
         txts.append(txt)
-        
+
 def plot_tsne(X, y):
   print('calculating tsne ...')
   proj = TSNE(random_state=RS).fit_transform(X)
